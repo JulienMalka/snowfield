011ec1a24e1fafbaf448cf4beacfe009f2c0d7f5
Julien Malka <julien.malka@me.com>
2021-12-23 11:00:26 +0100

    Added gtk module

--- machines/macintosh/home-julien.nix
+++ machines/macintosh/home-julien.nix
@@ -5,6 +5,7 @@
     luj.programs.neovim.enable = true;
     luj.programs.git.enable = true;
     luj.programs.ssh-client.enable = true;
+    luj.programs.gtk.enable = true;
     luj.i3.enable = true;
     luj.polybar.enable = true;
     home.packages = with pkgs; [
@@ -39,24 +40,9 @@
     };
 
 
-
-    gtk = {
-      enable = true;
-      theme = {
-        name = "Nordic";
-        package = pkgs.nordic;
-      };
-    };
-    qt = {
-      enable = true;
-      platformTheme = "gtk";
-    };
-
-
     fonts.fontconfig.enable = true;
 
     xsession.enable = true;
-
 
     home.stateVersion = "21.11";
 
