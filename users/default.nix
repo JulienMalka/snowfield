49299dc3b299cdfabc6abd30e6fe2369b34fb344
Julien Malka <julien.malka@me.com>
2021-12-28 16:42:30 +0100

    Lot of changes, playing with ssh keys

--- users/default.nix
+++ users/default.nix
@@ -1,20 +1,19 @@
 { config, pkgs, lib, ... }: {
 
-
   users.mutableUsers = false;
   users.defaultUserShell = pkgs.fish;
   sops.secrets.user-root-password.neededForUsers = true;
 
   programs.fish.enable = true;
 
-users.users.root = {
-        uid = config.ids.uids.root;
-        description = "System administrator";
-        home = "/root";
-        shell = lib.mkForce config.users.defaultUserShell;
-        group = "root";
-        passwordFile = config.sops.secrets.user-root-password.path;
-      };
+  users.users.root = {
+    uid = config.ids.uids.root;
+    description = "System administrator";
+    home = "/root";
+    shell = lib.mkForce config.users.defaultUserShell;
+    group = "root";
+    passwordFile = config.sops.secrets.user-root-password.path;
+  };
 
 
 }
--- users/julien.nix
+++ users/julien.nix
@@ -1,23 +1,21 @@
 { config, pkgs, lib, ... }: {
 
-
   sops.secrets.user-julien-password.neededForUsers = true;
-
 
   users.users.julien = {
     isNormalUser = true;
     home = "/home/julien";
-    extraGroups = [ "wheel" "docker" config.users.groups.keys.name]; 
+    extraGroups = [ "wheel" config.users.groups.keys.name]; 
     shell = pkgs.fish;
     passwordFile = config.sops.secrets.user-julien-password.path;
     openssh.authorizedKeys.keyFiles = [
       (pkgs.fetchurl {
         url = "https://github.com/JulienMalka.keys";
-        sha256 = "sha256-jx0/AAAeq5d6h1ytdUUnF/bMcn4h0UIKQCwzi3S5+YQ=";
+        sha256 = "sha256-nBgn7jOqi/nPHhTy3x/oirL+A4X2gbmwy1NXLZhV99M=";
       })
     ];
   };
 
-
   nix.allowedUsers = [ "julien" ];
+
 }
