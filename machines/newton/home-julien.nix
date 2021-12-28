ec84636e995e6b648fc0576e0278665de83629ac
Julien Malka <julien.malka@me.com>
2021-12-28 16:50:05 +0100

    Added newton ssh key

--- machines/newton/home-julien.nix
+++ machines/newton/home-julien.nix
@@ -1,5 +1,24 @@
 { pkgs, config, lib, ... }:
 {
+
+  sops.secrets.ssh-newton-pub = {
+    owner = "julien";
+    path = "/home/julien/.ssh/id_ed25519.pub";
+    mode = "0644";
+    format = "binary";
+    sopsFile = ../../secrets/ssh-newton-pub;
+  };
+
+  sops.secrets.ssh-newton-priv = {
+    owner = "julien";
+    path = "/home/julien/.ssh/id_ed25519";
+    mode = "0600";
+    format = "binary";
+    sopsFile = ../../secrets/ssh-newton-priv;
+  };
+
+
+
   luj.hmgr.julien = {
     luj.programs.neovim.enable = true;
     luj.programs.git.enable = true;
