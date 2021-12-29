3bce60caa4c8b2d35d95bf82e966d09c30001a26
Julien Malka <julien.malka@me.com>
2021-12-29 11:34:55 +0100

    Sending people to ssh jail

--- machines/newton/default.nix
+++ machines/newton/default.nix
@@ -21,6 +21,8 @@ in
   networking.interfaces.enp2s0f0.useDHCP = true;
   networking.interfaces.enp2s0f1.useDHCP = true;
 
+  services.fail2ban.enable = true;
+
   services.zfs.autoSnapshot.enable = true;
   services.zfs.autoScrub.enable = true;
 
