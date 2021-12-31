14724f5039098232dd7bc1cf2df12ae64fe1a5d2
Julien Malka <julien.malka@me.com>
2021-12-31 12:36:16 +0100

    New ip

--- machines/lisa/default.nix
+++ machines/lisa/default.nix
@@ -37,6 +37,13 @@
   services.fail2ban.enable = true;
 
   networking.hostName = "lisa";
+  networking.interfaces.ens20.useDHCP = false;
+  networking.interfaces.ens20.ipv4.addresses = [ {address = "212.129.40.11"; prefixLength = 32;} ];
+  #networking.defaultGateway = { interface = "ens20"; };
+  networking.localCommands = ''
+ip r del default
+ip r add default dev ens20
+'';
   networking.interfaces.ens18.useDHCP = true;
   networking.interfaces.ens19.useDHCP = false;
   networking.interfaces.ens19.ipv6.addresses = [{
