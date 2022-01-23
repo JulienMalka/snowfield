e1319341a8e9a458fb88ac75bc9fd1057ba2751b
Julien Malka <julien.malka@me.com>
2022-01-23 19:11:10 +0100

    Added mailserver and hedgedoc service

---
+++ modules/docs/default.nix
@@ -0,0 +1,55 @@
+{ lib, pkgs, config, ... }:
+with lib;
+let
+  cfg = config.luj.docs;
+  port = 3013;
+in
+{
+
+  options.luj.docs = {
+
+    enable = mkEnableOption "activate hedgedoc service";
+    nginx.enable = mkEnableOption "activate nginx";
+    nginx.subdomain = mkOption {
+      type = types.str;
+    };
+
+  };
+
+  config = mkIf cfg.enable (
+    mkMerge [{
+      services.hedgedoc = {
+        enable = true;
+        configuration = {
+          port = port;
+          db = {
+            dialect = "postgres";
+            host = "/run/postgresql";
+          };
+          domain = "docs.julienmalka.me";
+          protocolUseSSL = true;
+          allowFreeURL = true;
+          allowEmailRegister = false;
+          allowAnonymous = false;
+          allowAnonymousEdits = true;
+          allowGravatar = true;
+        };
+      };
+      services.postgresql = {
+        ensureDatabases = [ "hedgedoc" ];
+        ensureUsers = [
+          {
+            name = "hedgedoc";
+            ensurePermissions."DATABASE hedgedoc" = "ALL PRIVILEGES";
+          }
+        ];
+      };
+    }
+
+      (mkIf cfg.nginx.enable (mkSubdomain cfg.nginx.subdomain port))]);
+
+
+
+
+
+}
