a72bf5add670f0662a2c2e2507924ba4a55bcb07
Julien Malka <julien.malka@me.com>
2022-02-05 19:00:46 +0100

    Refactored mediaserver

--- modules/mediaserver/default.nix
+++ modules/mediaserver/default.nix
@@ -1,83 +1,97 @@
 { lib, pkgs, config, ... }:
 with lib;
 let cfg = config.luj.mediaserver;
-in {
+in
+{
   options.luj.mediaserver = {
     enable = mkEnableOption "enable the mediaserver";
+    tv.enable = mkEnableOption "enable the tv mediaserver";
+    music.enable = mkEnableOption "enable the music mediaserver";
   };
 
 
-  config = mkIf cfg.enable {
+  config = mkIf cfg.enable (
+    mkMerge [{
 
-    users.users.mediaserver = {
-      name = "mediaserver";
-      isNormalUser = true;
-      home = "/home/mediaserver";
-      group = config.users.groups.mediaserver.name;
-    };
+      users.users.mediaserver = {
+        name = "mediaserver";
+        isNormalUser = true;
+        home = "/home/mediaserver";
+        group = config.users.groups.mediaserver.name;
+      };
 
-    users.groups.mediaserver = {
-      name = "mediaserver";
-    };
+      users.groups.mediaserver = {
+        name = "mediaserver";
+      };
+
+      luj.jackett = {
+        enable = true;
+        user = "mediaserver";
+        group = "mediaserver";
+        nginx.enable = true;
+        nginx.subdomain = "jackett";
+      };
+
+      luj.flaresolverr.enable = true;
+
+      luj.transmission = {
+        enable = true;
+        user = "mediaserver";
+        group = "mediaserver";
+        nginx.enable = true;
+        nginx.subdomain = "downloads";
+      };
+
+    }
 
 
-    luj.sonarr = {
-      enable = true;
-      user = "mediaserver";
-      group = "mediaserver";
-      nginx.enable = true;
-      nginx.subdomain = "series";
-    };
+      (mkIf cfg.tv.enable {
 
-    luj.radarr = {
-      enable = true;
-      user = "mediaserver";
-      group = "mediaserver";
-      nginx.enable = true;
-      nginx.subdomain = "films";
-    };
+        luj.sonarr = {
+          enable = true;
+          user = "mediaserver";
+          group = "mediaserver";
+          nginx.enable = true;
+          nginx.subdomain = "series";
+        };
 
-    luj.lidarr = {
-      enable = true;
-      user = "mediaserver";
-      group = "mediaserver";
-      nginx.enable = true;
-      nginx.subdomain = "songs";
-    };
+        luj.radarr = {
+          enable = true;
+          user = "mediaserver";
+          group = "mediaserver";
+          nginx.enable = true;
+          nginx.subdomain = "films";
+        };
+        luj.jellyfin = {
+          enable = true;
+          user = "mediaserver";
+          group = "mediaserver";
+          nginx.enable = true;
+          nginx.subdomain = "tv";
+        };
 
-    luj.jellyfin = {
-      enable = true;
-      user = "mediaserver";
-      group = "mediaserver";
-      nginx.enable = true;
-      nginx.subdomain = "tv";
-    };
 
-    luj.jackett = {
-      enable = true;
-      user = "mediaserver";
-      group = "mediaserver";
-      nginx.enable = true;
-      nginx.subdomain = "jackett";
-    };
+      })
 
-    luj.flaresolverr.enable = true;
+      (mkIf cfg.music.enable {
+        luj.lidarr = {
+          enable = true;
+          user = "mediaserver";
+          group = "mediaserver";
+          nginx.enable = true;
+          nginx.subdomain = "songs";
+        };
 
-    luj.navidrome = {
-      enable = true;
-      user = "mediaserver";
-      group = "mediaserver";
-      nginx.enable = true;
-      nginx.subdomain = "music";
-    };
+        luj.navidrome = {
+          enable = true;
+          user = "mediaserver";
+          group = "mediaserver";
+          nginx.enable = true;
+          nginx.subdomain = "music";
+        };
 
-    luj.transmission = {
-      enable = true;
-      user = "mediaserver";
-      group = "mediaserver";
-      nginx.enable = true;
-      nginx.subdomain = "downloads";
-    };
-  };
 
+      })]);
 }
+
+
