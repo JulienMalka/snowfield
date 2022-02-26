c864b96f072345b466da80495afaf509bb2b74fa
Julien Malka <julien.malka@me.com>
2022-02-26 18:55:41 +0100

    Create VPN subdomains

--- modules/radarr/default.nix
+++ modules/radarr/default.nix
@@ -39,8 +39,9 @@ in
     }
 
       (mkIf cfg.nginx.enable (mkPrivateSubdomain cfg.nginx.subdomain port))
-
-    ]);
+      
+      (mkIf cfg.nginx.enable (mkVPNSubdomain cfg.nginx.subdomain port))]);
+ 
 
 
 
