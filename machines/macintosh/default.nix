5ed382323269e298a3b792ad065e79483cac5d4b
Julien Malka <julien.malka@me.com>
2022-02-26 23:50:06 +0100

    Added macintosh to vpn

--- machines/macintosh/default.nix
+++ machines/macintosh/default.nix
@@ -15,6 +15,7 @@
   networking.hostName = "macintosh";
   networking.networkmanager.enable = true;
 
+
   sound.enable = true;
   hardware.pulseaudio.enable = true;
   hardware.pulseaudio.support32Bit = true;
@@ -47,6 +48,35 @@
   programs.dconf.enable = true;
 
   system.stateVersion = "21.11"; 
+  networking.firewall.allowedUDPPorts = [ 51820 ];
+
+  networking.nameservers = lib.mkForce [ "10.100.0.2" ];
+  networking.networkmanager.insertNameservers = [ "10.100.0.2" ];
+  networking.resolvconf.dnsExtensionMechanism = false;
+  environment.etc."resolv.conf" = with lib; with pkgs; {
+    source = writeText "resolv.conf" ''
+      ${concatStringsSep "\n" (map (ns: "nameserver ${ns}") config.networking.nameservers)}
+      options edns0
+    '';
+  };
+
+  networking.wireguard.interfaces = {
+    wg0 = {
+      ips = [ "10.100.0.9/24" ];
+      listenPort = 51820;
+      privateKeyFile = "/root/wireguard-keys/private";
+
+      peers = [
+        {
+          allowedIPs = [ "10.100.0.0/24" ];
+          publicKey = "hz+h9Oque5h+Y/WzOUnai3e9UfIfDsvtqmQH0xycIzs=";
+          endpoint = "212.129.40.11:51820";
+          persistentKeepalive = 25;
+        }
+      ];
+    };
+  };
+
 
 }
 
