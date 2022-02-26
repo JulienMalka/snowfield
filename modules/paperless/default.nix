1e24eb44ab3f98351d7525a0a4abd47d97cf0dc4
Julien Malka <julien.malka@me.com>
2022-02-26 23:48:07 +0100

    Fixed paperless OCR langages

--- modules/paperless/default.nix
+++ modules/paperless/default.nix
@@ -1,12 +1,12 @@
 { lib, pkgs, config, ... }:
 with lib;
 let
-  cfg = config.luj.jackett;
+  cfg = config.luj.paperless;
   port = 28981;
 in
 {
 
-  options.luj.jackett = {
+  options.luj.paperless = {
     enable = mkEnableOption "activate paperless service";
 
     user = mkOption {
@@ -29,7 +29,7 @@ in
         user = cfg.user;
         mediaDir = "/home/julien/papers";
         extraConfig = {
-          PAPERLESS_OCR_LANGUAGE = "fre+eng";
+          PAPERLESS_OCR_LANGUAGE = "fra+eng";
           PAPERLESS_OCR_MODE = "redo";
           PAPERLESS_TIME_ZONE = "Europe/Paris";
         };
