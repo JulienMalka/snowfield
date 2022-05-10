5dc1c4ac793caf6789facfc4c94536a7fd7a9468
Julien Malka <julien@malka.sh>
2022-05-10 06:42:58 +0200

    Added one ssh key

--- users/julien.nix
+++ users/julien.nix
@@ -11,7 +11,7 @@
     openssh.authorizedKeys.keyFiles = [
       (pkgs.fetchurl {
         url = "https://github.com/JulienMalka.keys";
-        sha256 = "sha256-/i6WOEWBfXnRln9r6GCznoc47UzN+jInkWjTSqNafHI=";
+        sha256 = "sha256-ZTQpJO5/z/RIzvNpLBHv2GyCn8cvWsN5Hx3pd6s7RYY=";
       })
     ];
   };
