0ad9f1fefa353d894cf41cc4fae5486d866b04b5
Julien Malka <julien@malka.sh>
2022-10-12 09:54:19 +0200

    Updated keys

--- users/status.nix
+++ users/status.nix
@@ -8,7 +8,7 @@
     openssh.authorizedKeys.keyFiles = [
       (pkgs.fetchurl {
         url = "https://github.com/JulienMalka.keys";
-        sha256 = "sha256-ZTQpJO5/z/RIzvNpLBHv2GyCn8cvWsN5Hx3pd6s7RYY=";
+        sha256 = "sha256-1cbFnmpSt74KKcAthJswmBEFVR6cn9oVClK/Pu33OKQ=";
       })
     ];
   };
