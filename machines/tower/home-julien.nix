3f5da71fd4758e32cbde5d3acce8157e894652c7
Julien Malka <julien@malka.sh>
2023-01-07 18:39:38 +0100

    Added deploy pkg

--- machines/tower/home-julien.nix
+++ machines/tower/home-julien.nix
@@ -1,7 +1,10 @@
 { pkgs, lib, config, ... }:
 {
 
- luj.hmgr.julien = {
+
+  luj.hmgr.julien = {
+    home.packages = with pkgs; [ deploy-rs ];
+    home.stateVersion = "22.11";
     luj.programs.neovim.enable = true;
     luj.programs.ssh-client.enable = true;
     luj.programs.git.enable = true;
