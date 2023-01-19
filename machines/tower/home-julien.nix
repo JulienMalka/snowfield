9b88e0e621f70209ff388d72ec6efef852d6c58c
Julien Malka <julien@malka.sh>
2023-01-20 00:28:21 +0100

    Added more usefull tools

--- machines/tower/home-julien.nix
+++ machines/tower/home-julien.nix
@@ -3,7 +3,7 @@
 
 
   luj.hmgr.julien = {
-    home.packages = with pkgs; [ unstable.deploy-rs nixpkgs-review nixpkgs-fmt gh sops unstable.nix-eval-jobs ];
+    home.packages = with pkgs; [ unstable.deploy-rs nixpkgs-review nixpkgs-fmt gh sops unstable.nix-eval-jobs nix-bisect htop ];
     home.stateVersion = "22.11";
     luj.programs.neovim.enable = true;
     luj.programs.ssh-client.enable = true;
