d5adc5e41cee4d380b1f4def2a48c85015222da3
Julien Malka <julien@malka.sh>
2023-01-19 19:08:40 +0100

    Customizing home

--- machines/tower/home-julien.nix
+++ machines/tower/home-julien.nix
@@ -3,7 +3,7 @@
 
 
   luj.hmgr.julien = {
-    home.packages = with pkgs; [ unstable.deploy-rs ];
+    home.packages = with pkgs; [ unstable.deploy-rs nixpkgs-review nixpkgs-fmt gh sops unstable.nix-eval-jobs ];
     home.stateVersion = "22.11";
     luj.programs.neovim.enable = true;
     luj.programs.ssh-client.enable = true;
