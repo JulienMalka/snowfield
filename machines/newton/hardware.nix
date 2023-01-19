2ef286ebe3bfb87cdce210087d1f4ace9c803422
Julien Malka <julien@malka.sh>
2023-01-19 19:50:26 +0100

    Added newton, had to go around strange bug

--- machines/newton/hardware.nix
+++ machines/newton/hardware.nix
@@ -18,12 +18,17 @@
       enable = true;
       port = 2222;
       hostKeys = [ /boot-1/initrd-ssh-key /boot-2/initrd-ssh-key ];
-      authorizedKeys = lib.splitString "\n"
-        (builtins.readFile (pkgs.fetchurl {
-          url = "https://github.com/JulienMalka.keys";
-          sha256 = "sha256-xa8TyZb28M4cwdTDAwqN608xdXZBf8PtvNFk1JxE2lQ=";
-        }));
-
+      authorizedKeys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM9Uzb7szWlux7HuxLZej9cBR5MhLz/vaAPPfSoozt2k"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDCKfPoMNrnyNWH6J1OvQ+n1rvSS9Sc2iZf6E1JQC+L4"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIESMWr29i3rhj32oLV3DKe57YI+jvNaKjZhhpq6dEjsn"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJOCKgHRHAJDSgKqYNfWboL04mnEOM0m0K3TGxBhBNDR"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOpGHx430EpJmbtJc8+lF1CpQ1gXeHT9OeZ08O8yzohF"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEaCGndojnmS5IoqHVMEPRfKuBZotMyqo7wNkAZJWigp"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINqqbb91oMRg0L5kcljMhuKi4l2TjE/JKJQwcFVahDJH"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILxfFq8wx5Bet5Q0gI28/lc9ryYYFQelpZdPPdzxGBbA"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGa+7n7kNzb86pTqaMn554KiPrkHRGeTJ0asY1NjSbpr"
+      ];
     };
     postCommands = ''
       zpool import zroot
