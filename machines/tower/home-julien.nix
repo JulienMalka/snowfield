e66246e094e58eeb0d76eacc214e0db789c82dfa
Julien Malka <julien@malka.sh>
2023-02-13 13:20:29 +0100

    feat: added direnv on tower

--- machines/tower/home-julien.nix
+++ machines/tower/home-julien.nix
@@ -8,5 +8,14 @@
     luj.programs.neovim.enable = true;
     luj.programs.ssh-client.enable = true;
     luj.programs.git.enable = true;
+
+
+    programs.direnv = {
+      enable = true;
+      enableFishIntegration = true;
+      nix-direnv.enable = true;
+    };
+
+
   };
 }
