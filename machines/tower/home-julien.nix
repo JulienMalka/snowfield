a5a39b4cfe1e0eef0912b8afa16ad3e95dbeeead
Julien Malka <julien@malka.sh>
2023-04-18 14:13:15 +0200

    tower: added jq in user packages

--- machines/tower/home-julien.nix
+++ machines/tower/home-julien.nix
@@ -3,7 +3,7 @@
 
 
   luj.hmgr.julien = {
-    home.packages = with pkgs; [ unstable.deploy-rs nixpkgs-review nixpkgs-fmt gh sops unstable.nix-eval-jobs nix-bisect htop hydrasect tmux lazygit ];
+    home.packages = with pkgs; [ unstable.deploy-rs nixpkgs-review nixpkgs-fmt gh sops unstable.nix-eval-jobs nix-bisect htop hydrasect tmux lazygit jq ];
     home.stateVersion = "22.11";
     luj.programs.neovim.enable = true;
     luj.programs.ssh-client.enable = true;
