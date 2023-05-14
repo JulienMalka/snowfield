7444fe0c6bbdc40acd73b09d6a263a4e5bd86cc4
Julien Malka <julien@malka.sh>
2023-05-14 14:21:38 +0200

    empty hm for bin-cache

--- machines/bin-cache/home-julien.nix
+++ machines/bin-cache/home-julien.nix
@@ -1,9 +1,4 @@
 { pkgs, lib, config, ... }:
 {
-  luj.hmgr.julien = {
-    home.stateVersion = "22.11";
-    luj.programs.neovim.enable = true;
-    luj.programs.ssh-client.enable = true;
-    luj.programs.git.enable = true;
-  };
+  luj.hmgr.julien = { };
 }
