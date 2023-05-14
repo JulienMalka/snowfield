ff3ecef36bca262ba297814e694adee08d316bfb
Julien Malka <julien@malka.sh>
2023-05-14 13:13:36 +0200

    bin-cache: fix wrong bootloader

--- machines/bin-cache/default.nix
+++ machines/bin-cache/default.nix
@@ -9,8 +9,10 @@
       ../../users/default.nix
     ];
 
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/sda";
+  boot.loader.grub.useOSProber = true;
 
   networking.hostName = "bin-cache";
 
