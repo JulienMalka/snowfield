9bdd84bbf9e7e0dd7e0b7ced2307bf617a17babe
Julien Malka <julien@malka.sh>
2023-06-10 17:50:04 +0200

    resolved on macintosh

--- machines/macintosh/default.nix
+++ machines/macintosh/default.nix
@@ -1,7 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, lib, inputs, ... }:
 
 {
@@ -15,12 +11,11 @@
       inputs.nixos-apple-silicon.nixosModules.apple-silicon-support
     ];
 
-  # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = false;
   nixpkgs.config.allowUnsupportedSystem = false;
   networking.hostName = "macintosh"; # Define your hostname.
-  # Pick only one of the below networking options.
+
   networking.wireless.enable = false;
 
   hardware.asahi.addEdgeKernelConfig = true;
@@ -34,17 +29,14 @@
     LIBSEAT_BACKEND = "logind";
   };
 
-  programs.fish.shellInit = ''
-    if test -z (pgrep ssh-agent)
-    eval (ssh-agent -c) > /dev/null
-    set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
-    set -Ux SSH_AGENT_PID $SSH_AGENT_PID
-    set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
-    end
-  '';
-
+  services.xserver.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
   services.tailscale.enable = true;
   networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
+
+  networking.networkmanager.dns = "systemd-resolved";
+  services.resolved.enable = true;
+
 
   time.timeZone = "Europe/Paris";
 
@@ -88,8 +80,6 @@
 
   security.pam.services.swaylock = { };
 
-  services.gnome3.gnome-keyring.enable = true;
-  security.pam.services.login.enableGnomeKeyring = true;
   programs.ssh.startAgent = true;
 
   programs.adb.enable = true;
