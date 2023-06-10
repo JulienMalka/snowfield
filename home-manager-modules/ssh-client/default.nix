0dfa155713afaa16b1b040c48a39c96c627eb74d
Julien Malka <julien@malka.sh>
2023-06-10 19:54:28 +0200

    changed ssh config

--- home-manager-modules/ssh-client/default.nix
+++ home-manager-modules/ssh-client/default.nix
@@ -12,52 +12,13 @@ with lib;
     programs.ssh = {
       enable = true;
       matchBlocks = {
-        newton = {
-          hostname = "newton.julienmalka.me";
-          user = "julien";
-          port = 45;
-        };
-        lisa = {
-          hostname = "lisa.julienmalka.me";
-          user = "julien";
-          port = 45;
-        };
-        newton-init = {
-          hostname = "newton.julienmalka.me";
-          user = "root";
-          port = 2222;
-        };
         sas = {
           hostname = "sas.eleves.ens.fr";
           user = "jmalka";
         };
-        lambda = {
-          hostname = "lambda.julienmalka.me";
-          user = "root";
-          port = 45;
-        };
-        tower = {
-          hostname = "tower.julienmalka.me";
+        "*" = {
+          hostname = "%h.luj";
           user = "julien";
-          port = 45;
-        };
-        saumon = {
-          hostname = "saumon.julienmalka.me";
-          user = "julien";
-        };
-        stockly = {
-          hostname = "charybdis.stockly.tech";
-          user = "julien_malka";
-          port = 23;
-        };
-        router = {
-          hostname = "vms.julienmalka.me";
-          user = "julien";
-        };
-        core-security = {
-          hostname = "192.168.0.175";
-          user = "julien";
-          proxyJump = "router";
           port = 45;
         };
       };
