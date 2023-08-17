0223a3a222a094732a9d55e3d6c894d0b868abd4
Julien Malka <julien@malka.sh>
2023-08-17 18:00:53 +0200

    removed macintosh keys from x2100

--- machines/x2100/home-julien.nix
+++ machines/x2100/home-julien.nix
@@ -1,22 +1,6 @@
 { pkgs, lib, config, ... }:
 {
 
-  sops.secrets.ssh-macintosh-pub = {
-    owner = "julien";
-    path = "/home/julien/.ssh/id_ed25519.pub";
-    mode = "0644";
-    format = "binary";
-    sopsFile = ../../secrets/ssh-macintosh-pub;
-  };
-
-  sops.secrets.ssh-macintosh-priv = {
-    owner = "julien";
-    path = "/home/julien/.ssh/id_ed25519";
-    mode = "0600";
-    format = "binary";
-    sopsFile = ../../secrets/ssh-macintosh-priv;
-  };
-
   luj.hmgr.julien =
     {
       home.stateVersion = "22.11";
