e310544bdfc5944462fd42cfe321b0ec9e20f15b
Julien Malka <julien@malka.sh>
2023-09-10 15:19:18 +0200

    added audio support

--- users/julien.nix
+++ users/julien.nix
@@ -5,7 +5,7 @@
   users.users.julien = {
     isNormalUser = true;
     home = "/home/julien";
-    extraGroups = [ "wheel" config.users.groups.keys.name "filerun" "networkmanager" "davfs2" "adbusers" ];
+    extraGroups = [ "wheel" config.users.groups.keys.name "filerun" "networkmanager" "davfs2" "adbusers" "audio" ];
     shell = pkgs.fish;
     passwordFile = config.sops.secrets.user-julien-password.path;
   };
