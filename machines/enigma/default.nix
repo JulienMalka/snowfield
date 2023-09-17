8479cfe921cba75b2f3dda97470f48c1cc489c0f
Julien Malka <julien@malka.sh>
2023-09-17 23:29:19 +0200

    bootstrapped enigma

---
+++ machines/enigma/default.nix
@@ -0,0 +1,102 @@
+{ config, pkgs, lib, inputs, ... }:
+
+{
+  imports =
+    [
+      ./hardware.nix
+      ./home-julien.nix
+      ../../users/julien.nix
+      ../../users/default.nix
+    ];
+
+  networking.hostName = "enigma";
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+
+  services.tailscale.enable = true;
+  networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
+
+  networking.networkmanager.dns = "systemd-resolved";
+  services.resolved.enable = true;
+
+
+services.xserver.enable = true;
+
+services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+# Enable OpenGL
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+#    driSupport32Bit = true;
+  };
+
+  # Load nvidia driver for Xorg and Wayland
+  services.xserver.videoDrivers = ["nvidia"];
+
+  hardware.nvidia = {
+
+    # Modesetting is required.
+    modesetting.enable = true;
+
+    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+    powerManagement.enable = false;
+    # Fine-grained power management. Turns off GPU when not in use.
+    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+    powerManagement.finegrained = false;
+
+    # Use the NVidia open source kernel module (not to be confused with the
+    # independent third-party "nouveau" open source driver).
+    # Support is limited to the Turing and later architectures. Full list of 
+    # supported GPUs is at: 
+    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+    # Only available from driver 515.43.04+
+    # Do not disable this unless your GPU is unsupported or if you have a good reason to.
+    open = true;
+
+    # Enable the Nvidia settings menu,
+	# accessible via `nvidia-settings`.
+    nvidiaSettings = true;
+
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.beta;
+  };
+
+
+boot.initrd.kernelModules = [ "nvidia" ];
+boot.extraModulePackages = [ config.boot.kernelPackages.nvidia_x11 ];
+
+  time.timeZone = "Europe/Paris";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+  console = {
+    font = "Lat2-Terminus16";
+    useXkbConfig = true; # use xkbOptions in tty.
+  };
+
+  programs.dconf.enable = true;
+
+  security.polkit.enable = true;
+
+
+  environment.systemPackages = with pkgs; [
+    tailscale
+    brightnessctl
+    sbctl
+  ];
+
+  services.printing.enable = true;
+  services.avahi.enable = true;
+  services.avahi.nssmdns = true;
+  # for a WiFi printer
+  services.avahi.openFirewall = true;
+
+  system.stateVersion = "23.05";
+
+}
+
+
+
