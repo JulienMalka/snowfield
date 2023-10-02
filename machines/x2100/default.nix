70c9c62418b202dc027a4cef680de266c3a709ff
Julien Malka <julien@malka.sh>
2023-10-02 15:08:40 +0200

    x2100: add bluetooth and tpm stuff

--- machines/x2100/default.nix
+++ machines/x2100/default.nix
@@ -81,6 +81,13 @@
 
   services.tlp.enable = true;
 
+  security.tpm2.enable = true;
+  security.tpm2.pkcs11.enable = true; # expose /run/current-system/sw/lib/libtpm2_pkcs11.so
+  security.tpm2.tctiEnvironment.enable = true; # TPM2TOOLS_TCTI and TPM2_PKCS11_TCTI env variables
+  users.users.julien.extraGroups = [ "tss" ]; # tss group has access to TPM devices
+
+  hardware.bluetooth.enable = true;
+
   environment.systemPackages = with pkgs; [
     tailscale
     brightnessctl
