e2f2600a75fa40cbec2dad3bcf649c2373eae459
Julien Malka <julien@malka.sh>
2023-11-09 10:50:32 +0100

    kitty: better config

--- home-manager-modules/kitty/default.nix
+++ home-manager-modules/kitty/default.nix
@@ -5,7 +5,7 @@ in
 with lib;
 {
   options.luj.programs.kitty = {
-    enable = mkEnableOption "Enable SwayWM";
+    enable = mkEnableOption "Enable Kitty";
   };
 
   config = mkIf cfg.enable {
@@ -16,6 +16,8 @@ with lib;
         wayland_titlebar_color = "background";
         background_opacity = "0.96";
         shell_integration = "no-cursor";
+        window_padding_width = 4;
+        confirm_os_window_close = 0;
       };
       font = {
         name = "FiraCode Nerd Font Mono Reg";
