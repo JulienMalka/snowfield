bc60a5c4dababb6eaa53f69434a6fd3957d37acf
Julien Malka <julien@malka.sh>
2023-11-24 17:57:45 +0100

    update home manager release

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.05";
+      url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
