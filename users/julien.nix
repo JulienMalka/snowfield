5d8e1a494d356f9a21f42c949b415e40feba7139
Julien Malka <julien@malka.sh>
2024-03-02 17:29:41 +0100

    add julien to pipewire group

--- users/julien.nix
+++ users/julien.nix
@@ -5,7 +5,7 @@
   users.users.julien = {
     isNormalUser = true;
     home = "/home/julien";
-    extraGroups = [ "wheel" config.users.groups.keys.name "filerun" "networkmanager" "davfs2" "adbusers" "audio" ];
+    extraGroups = [ "wheel" config.users.groups.keys.name "filerun" "networkmanager" "davfs2" "adbusers" "audio" "pipewire" "dialout" ];
     shell = pkgs.zsh;
     hashedPasswordFile = config.sops.secrets.user-julien-password.path;
   };
