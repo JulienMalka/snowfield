91c30d5cbf64f43d51560a1b23cfeea19dd6b5b4
Julien Malka <julien@malka.sh>
2024-03-30 12:57:27 +0100

    remove doom emacs

--- modules/hmgr/default.nix
+++ modules/hmgr/default.nix
@@ -14,7 +14,7 @@ with lib;
       lib.mapAttrs
         (name: value:
           {
-            imports = with builtins; (map (x: ../../home-manager-modules + "/${x}/default.nix") (attrNames (readDir ../../home-manager-modules))) ++ [ inputs.nix-doom-emacs.hmModule ];
+            imports = with builtins; (map (x: ../../home-manager-modules + "/${x}/default.nix") (attrNames (readDir ../../home-manager-modules)));
             home.username = "${name}";
             home.homeDirectory = "/home/${name}";
             home.stateVersion = "21.05";
