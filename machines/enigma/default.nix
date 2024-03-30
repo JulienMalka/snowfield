b1f5ec785bebe54b6291ec52e094827e42a77f1d
Julien Malka <julien@malka.sh>
2024-03-30 23:19:34 +0100

    fix warning

--- machines/enigma/default.nix
+++ machines/enigma/default.nix
@@ -155,7 +155,7 @@
 
   services.printing.enable = true;
   services.avahi.enable = true;
-  services.avahi.nssmdns = true;
+  services.avahi.nssmdns4 = true;
   # for a WiFi printer
   services.avahi.openFirewall = true;
 
--- machines/x2100/default.nix
+++ machines/x2100/default.nix
@@ -120,7 +120,7 @@
 
   services.printing.enable = true;
   services.avahi.enable = true;
-  services.avahi.nssmdns = true;
+  services.avahi.nssmdns4 = true;
   # for a WiFi printer
   services.avahi.openFirewall = true;
 
