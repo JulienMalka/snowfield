a0cf4af2e0a8b7e4a7e4a6c9dbb0549c4d1220d1
Julien Malka <julien@malka.sh>
2024-04-05 00:33:53 +0200

    add patch to fix step-cli

--- patches/default.nix
+++ patches/default.nix
@@ -5,6 +5,11 @@
       id = "295736";
       hash = "sha256-JKuzNtI1X7sFT2raEC9UOf4feGdS8bg5uG2r8TMtFag=";
     }
+    {
+      # step-cli: fix hash of release 0.26.0
+      id = "301623";
+      hash = "sha256-J8RTS4isRis/i4hhqZSWLPcCWq2TxXy9Lrj841GhAo8=";
+    }
 
   ];
 }
