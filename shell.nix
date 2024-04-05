005bea5ae2c4cb4e2db3c860ade7835741f56e3a
Julien Malka <julien@malka.sh>
2024-04-05 19:29:45 +0200

    add agenix to shell

--- shell.nix
+++ shell.nix
@@ -2,9 +2,10 @@ let
   inputs = import ./deps;
   pkgs = import inputs.nixpkgs { };
   nixos-anywhere = pkgs.callPackage "${inputs.nixos-anywhere}/src/default.nix" { };
+  agenix = pkgs.callPackage "${inputs.agenix}/pkgs/agenix.nix" { };
 in
 pkgs.mkShell {
-  nativeBuildInputs = with pkgs; [ colmena npins nixos-anywhere ];
+  nativeBuildInputs = with pkgs; [ colmena npins nixos-anywhere agenix ];
 }
 
 
