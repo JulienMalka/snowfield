e6307c4e45c7a77cb356771cf077b35db11fa95f
Julien Malka <julien@malka.sh>
2024-05-01 19:10:29 +0200

    chore: remove hardcoded nix version

--- modules/nix/default.nix
+++ modules/nix/default.nix
@@ -1,4 +1,9 @@
-{ pkgs, config, lib, inputs, ... }:
+{
+  config,
+  lib,
+  inputs,
+  ...
+}:
 let
   cfg = config.luj.nix;
 in
@@ -8,32 +13,26 @@ with lib;
     enable = mkEnableOption "Enable nix experimental";
   };
 
-  config = mkIf cfg.enable
-    {
-      nixpkgs.config.allowUnfree = true;
-      nix = {
-        package = pkgs.unstable.nix;
-        extraOptions = ''
-          experimental-features = nix-command flakes
-        '';
-        nixPath = [
-          "nixpkgs=${inputs.nixpkgs}"
-          "nixos=${inputs.nixpkgs}"
+  config = mkIf cfg.enable {
+    nixpkgs.config.allowUnfree = true;
+    nix = {
+      #package = pkgs.unstable.nix;
+      extraOptions = ''
+        experimental-features = nix-command flakes
+      '';
+      nixPath = [
+        "nixpkgs=${inputs.nixpkgs}"
+        "nixos=${inputs.nixpkgs}"
+      ];
+      settings = {
+        builders-use-substitutes = true;
+        auto-optimise-store = true;
+        substituters = [
+          "https://cache.nixos.org"
+          "https://cache.saumon.network/attic-exec?priority=100"
         ];
-        settings =
-          {
-            builders-use-substitutes = true;
-            auto-optimise-store = true;
-            substituters = [
-              "https://cache.nixos.org"
-              "https://cache.saumon.network/attic-exec?priority=100"
-            ];
-            trusted-public-keys = [
-              "attic-exec:W1PQ0txRf4qpCIlNLscD/Xw1GwGoBij73JSum/I8Xt4="
-            ];
-          };
-
+        trusted-public-keys = [ "attic-exec:W1PQ0txRf4qpCIlNLscD/Xw1GwGoBij73JSum/I8Xt4=" ];
       };
-
     };
+  };
 }
