ca7fdee9a69c055062b87d0b7dd4d2989ea7b1b6
Julien Malka <julien@malka.sh>
2024-05-01 19:09:22 +0200

    chore: removed merged patches

--- patches/default.nix
+++ patches/default.nix
@@ -1,16 +1,1 @@
-{
-  "nixos-unstable" = [
-    {
-      # nixos/bcachefs: support unlock with clevis in systemd stage 1
-      id = "295736";
-      hash = "sha256-JKuzNtI1X7sFT2raEC9UOf4feGdS8bg5uG2r8TMtFag=";
-    }
-    {
-      # step-cli: fix hash of release 0.26.0
-      id = "301623";
-      hash = "sha256-J8RTS4isRis/i4hhqZSWLPcCWq2TxXy9Lrj841GhAo8=";
-    }
-
-  ];
-}
-
+{ }
