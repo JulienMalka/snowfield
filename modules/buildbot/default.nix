fee0e589bb1784b23a6155bff4165d696051ab6f
Julien Malka <julien@malka.sh>
2024-05-22 23:31:08 +0200

    feat: enable badges on buildbot

--- modules/buildbot/default.nix
+++ modules/buildbot/default.nix
@@ -67,5 +67,34 @@ in
       enable = true;
       workerPasswordFile = config.age.secrets.buildbot-nix-worker-password.path;
     };
+    services.buildbot-master = {
+      pythonPackages = _: [
+        pkgs.buildbot-plugins.badges
+        pkgs.buildbot-plugins.www
+      ];
+      extraConfig = ''
+        c["www"].update({"plugins": {"badges": {
+          "left_pad"  : 5,
+          "left_text": "Build Status",  # text on the left part of the image
+          "left_color": "#555",  # color of the left part of the image
+          "right_pad" : 5,
+          "border_radius" : 5, # Border Radius on flat and plastic badges
+          # style of the template availables are "flat", "flat-square", "plastic"
+          "template_name": "flat.svg.j2",  # name of the template
+          "font_face": "DejaVu Sans",
+          "font_size": 11,
+          "color_scheme": {  # color to be used for right part of the image
+            "exception": "#007ec6", 
+            "failure": "#e05d44",    
+            "retry": "#007ec6",      
+            "running": "#007ec6",   
+            "skipped": "a4a61d",   
+            "success": "#4c1",      
+            "unknown": "#9f9f9f",   
+            "warnings": "#dfb317"   
+            } 
+        }}})
+      '';
+    };
   };
 }
