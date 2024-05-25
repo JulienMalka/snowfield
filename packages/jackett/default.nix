41288418294208aa40c94831c8e7136528df350d
Julien Malka <julien@malka.sh>
2024-05-25 17:48:08 +0200

    chore: vendor jackett package

---
+++ packages/jackett/default.nix
@@ -0,0 +1,52 @@
+{
+  lib,
+  stdenv,
+  buildDotnetModule,
+  fetchFromGitHub,
+  dotnetCorePackages,
+  openssl,
+  mono,
+}:
+
+buildDotnetModule rec {
+  pname = "jackett";
+  version = "0.21.2688";
+
+  src = fetchFromGitHub {
+    owner = pname;
+    repo = pname;
+    rev = "v${version}";
+    hash = "sha512-ofKCk1vWPbn3dFQ8qt7ieTXzkyFUoRGWsKGLpBZGEduX71aGX1M8wri27nt27FShb1Q7gVbGqUq6RHQI4bwnlQ==";
+  };
+
+  projectFile = "src/Jackett.Server/Jackett.Server.csproj";
+  nugetDeps = ./deps.nix;
+
+  dotnet-runtime = dotnetCorePackages.aspnetcore_6_0;
+
+  dotnetInstallFlags = [ "-p:TargetFramework=net6.0" ];
+
+  runtimeDeps = [ openssl ];
+
+  doCheck = !(stdenv.isDarwin && stdenv.isAarch64); # mono is not available on aarch64-darwin
+  nativeCheckInputs = [ mono ];
+  testProjectFile = "src/Jackett.Test/Jackett.Test.csproj";
+
+  postFixup = ''
+    # For compatibility
+    ln -s $out/bin/jackett $out/bin/Jackett || :
+    ln -s $out/bin/Jackett $out/bin/jackett || :
+  '';
+
+  meta = with lib; {
+    description = "API Support for your favorite torrent trackers";
+    homepage = "https://github.com/Jackett/Jackett/";
+    changelog = "https://github.com/Jackett/Jackett/releases/tag/v${version}";
+    license = licenses.gpl2Only;
+    maintainers = with maintainers; [
+      edwtjo
+      nyanloutre
+      purcell
+    ];
+  };
+}
