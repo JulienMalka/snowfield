8ad260f7a57563d5bef6563230eea1aac9cf0527
Julien Malka <julien@malka.sh>
2024-07-12 15:50:49 +0200

    chore: silence git-hooks nixfmt warning

--- shell.nix
+++ shell.nix
@@ -4,21 +4,29 @@ let
   nixos-anywhere = pkgs.callPackage "${inputs.nixos-anywhere}/src/default.nix" { };
   agenix = pkgs.callPackage "${inputs.agenix}/pkgs/agenix.nix" { };
   bootstrap = import scripts/bootstrap-machine.nix;
-  pre-commit-hook = (import inputs.git-hooks).run {
-    src = ./.;
+  pre-commit-hook =
+    (import (
+      pkgs.applyPatches {
+        name = "patched-git-hooks";
+        src = inputs.git-hooks;
+        patches = [ ./patches/hooks-correct-nixfmt.patch ];
+      }
+    )).run
+      {
+        src = ./.;
 
-    hooks = {
-      statix.enable = true;
-      deadnix.enable = true;
-      rfc101 = {
-        enable = true;
-        name = "RFC-101 formatting";
-        entry = "${pkgs.lib.getExe pkgs.nixfmt-rfc-style}";
-        files = "\\.nix$";
+        hooks = {
+          statix.enable = true;
+          deadnix.enable = true;
+          rfc101 = {
+            enable = true;
+            name = "RFC-101 formatting";
+            entry = "${pkgs.lib.getExe pkgs.nixfmt-rfc-style}";
+            files = "\\.nix$";
+          };
+          commitizen.enable = true;
+        };
       };
-      commitizen.enable = true;
-    };
-  };
 in
 pkgs.mkShell {
   nativeBuildInputs = with pkgs; [
