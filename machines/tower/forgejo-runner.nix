cd55f504df0931c4ce8ad0d6fab570876a1bd764
Julien Malka <julien@malka.sh>
2024-08-03 11:06:14 +0200

    feat: add forgejo runner on tower

---
+++ machines/tower/forgejo-runner.nix
@@ -0,0 +1,27 @@
+{ pkgs, config, ... }:
+{
+  age.secrets.forgejo_runners-token_file.file = ../../secrets/forgejo_runners-token_file.age;
+  nix.settings.allowed-users = [ "gitea-runner" ];
+  nix.settings.trusted-users = [ "gitea-runner" ];
+
+  services.gitea-actions-runner = {
+    package = pkgs.forgejo-runner;
+    instances = {
+      native = {
+        enable = true;
+        url = "https://git.luj.fr";
+        name = "native";
+        labels = [ "native:host" ];
+        tokenFile = config.age.secrets.forgejo_runners-token_file.path;
+        hostPackages = with pkgs; [
+          lix
+          nodejs
+          git
+          bash
+          coreutils
+          curl
+        ];
+      };
+    };
+  };
+}
