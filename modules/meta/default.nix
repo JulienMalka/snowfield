b6480b72ecb1c60078f0f58b55ebc0ad4a87232d
Julien Malka <julien@malka.sh>
2024-08-10 17:56:12 +0200

    feat: add meta module

---
+++ modules/meta/default.nix
@@ -0,0 +1,43 @@
+{ lib, ... }:
+{
+  options.machine.meta = lib.mkOption {
+    description = "Machine metadata";
+    type =
+      with lib;
+      types.submodule (
+        { name, ... }:
+        {
+          freeformType =
+            with types;
+            oneOf [
+              str
+              attrs
+            ];
+          options = {
+            hostname = mkOption {
+              description = "The machine's hostname";
+              type = types.str;
+              default = name;
+              readOnly = true;
+            };
+            sshPort = mkOption {
+              description = "The port for the ssh server of the machine";
+              type = types.int;
+              default = 45;
+            };
+            sshUser = mkOption {
+              description = "The user for ssh connection to the machine";
+              default = "julien";
+            };
+            tld = mkOption {
+              description = "tld for local addressing of the machine";
+              default = "luj";
+            };
+
+          };
+        }
+      );
+    default = { };
+  };
+
+}
