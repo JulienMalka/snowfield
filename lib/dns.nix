d7e02254872a1af5cb9515ff828aab22d3a87a9c
Julien Malka <julien@malka.sh>
2024-08-12 18:12:37 +0200

    feat: add dns lib

---
+++ lib/dns.nix
@@ -0,0 +1,39 @@
+{ lib, dnsLib, ... }:
+
+with lib;
+
+rec {
+
+  hasSuffix' = flip strings.hasSuffix;
+
+  domainToZone = allowedDomains: domain: (findFirst (hasSuffix' domain) null allowedDomains);
+
+  filterElligibleDomains = allowedDomains: domain: domainToZone allowedDomains domain != null;
+
+  domainsFromConfiguration =
+    allowedDomains: config:
+    filter (filterElligibleDomains allowedDomains) (attrNames config.services.nginx.virtualHosts);
+
+  ipsToRecord =
+    ipType: ipValue:
+    with dnsLib.combinators;
+    if ipType == "ipv4" then { A = [ ipValue ]; } else { AAAA = [ ipValue ]; };
+
+  domainToRecords =
+    domain: machineMeta: isVPNDomain:
+    with dnsLib.combinators;
+    (optionalAttrs isVPNDomain (
+      mapAttrsWithMerge (n: v: nameValuePair domain (ipsToRecord n v)) machineMeta.ips.vpn
+    ))
+    // (optionalAttrs (!isVPNDomain) (
+      mapAttrsWithMerge (n: v: nameValuePair domain (ipsToRecord n v)) machineMeta.ips.public
+    ));
+
+  getDomainPrefix =
+    allowedDomains: domain:
+    let
+      zone = domainToZone allowedDomains domain;
+    in
+    strings.removeSuffix ".${zone}" domain;
+
+}
