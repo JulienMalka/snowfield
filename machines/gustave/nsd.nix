1db71f86db2d251b9396f3a019630a11992a5d2b
Julien Malka <julien@malka.sh>
2024-08-12 18:15:30 +0200

    feat: add dns module

---
+++ machines/gustave/nsd.nix
@@ -0,0 +1,48 @@
+{
+  config,
+  lib,
+  inputs,
+  nixosConfigurations,
+  ...
+}:
+let
+  zonesToList = lib.mapAttrsToList (name: value: { ${name} = value; });
+  zonesFromConfig = lib.mkMerge (
+    lib.fold (elem: acc: acc ++ (zonesToList elem.config.machine.meta.zones)) [ ] (
+      lib.attrValues nixosConfigurations
+    )
+  );
+  dnsLib = (import inputs.dns).lib;
+  evalZones =
+    zones:
+    (lib.evalModules {
+      modules = [
+        {
+          options = {
+            zones = lib.mkOption {
+              type = lib.types.attrsOf dnsLib.types.zone;
+              description = "DNS zones";
+            };
+          };
+          config = {
+            inherit zones;
+          };
+        }
+      ];
+    }).config.zones;
+
+in
+
+{
+  services.nsd = {
+    enable = true;
+    interfaces = [
+      config.machine.meta.ips.public.ipv4
+      config.machine.meta.ips.public.ipv6
+    ];
+    zones = lib.mapAttrs (_: value: {
+      data = builtins.toString value;
+      provideXFR = [ "192.168.0.0/21 NOKEY" ];
+    }) (evalZones zonesFromConfig);
+  };
+}
---
+++ modules/dns/default.nix
@@ -0,0 +1,61 @@
+{
+  lib,
+  config,
+  inputs,
+  ...
+}:
+let
+  cfg = config.machine.meta;
+  allowedDomains = [
+    "luj.fr"
+    "julienmalka.me"
+    "malka.family"
+    "luj"
+    "luj-static.page"
+  ];
+
+  isVPNDomain = domain: lib.dns.domainToZone [ "luj" ] domain != null;
+  dnsLib = (import inputs.dns).lib;
+  SOA = {
+    nameServer = "ns";
+    adminEmail = "dns@malka.sh";
+    serial = 0;
+  };
+  defaults = {
+    inherit SOA;
+  };
+in
+with lib;
+{
+
+  options = {
+    machine.meta.zones = mkOption {
+      type = types.attrsOf dnsLib.types.zone;
+      default = { };
+    };
+  };
+
+  config =
+    let
+      # list of domains that are defined in the current configuration throught virtualHosts
+      domains = lib.dns.domainsFromConfiguration allowedDomains config;
+      # AttrSet domain -> { records }
+      recordsPerDomain = map (
+        domain:
+        mapAttrs' (
+          n: v:
+          nameValuePair (lib.dns.domainToZone allowedDomains n) (
+            let
+              subdomain = lib.dns.getDomainPrefix allowedDomains n;
+            in
+            (if elem subdomain allowedDomains then v else { subdomains."${subdomain}" = v; }) // defaults
+          )
+        ) (lib.dns.domainToRecords domain cfg (isVPNDomain domain))
+      ) domains;
+    in
+
+    {
+      machine.meta.zones = lib.mkMerge recordsPerDomain;
+    };
+
+}
