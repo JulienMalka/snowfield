4d5f3667bf70c970a1ac1a393f1890f22006bd26
Julien Malka <julien@malka.sh>
2024-09-02 14:21:25 +0200

    chore: add patch to fix hyprpaper

--- patches/default.nix
+++ patches/default.nix
@@ -5,5 +5,10 @@
       id = "338836";
       hash = "sha256-QVKF0UIYK5uAq3w1w+9kWwmm1bvMQl7PMgeZ5FkSUFs=";
     }
+    {
+      # hyprpaper: fix build
+      id = "338841";
+      hash = "sha256-IgxbVYn8lVUowcTM/v8dBTdptsivg6biDC+jQWFsaQY=";
+    }
   ];
 }
