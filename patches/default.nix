e96ab11f1abe0fe8d30b7ca4174713d863dac84d
Julien Malka <julien@malka.sh>
2024-09-02 14:11:58 +0200

    fix: add patch for xdg-desktop-portal-hyperland

--- patches/default.nix
+++ patches/default.nix
@@ -1,1 +1,9 @@
-{ }
+{
+  "nixos-unstable" = [
+    {
+      # xdg-desktop-portal-hyprland: fix build
+      id = "338836";
+      hash = "sha256-JKuzftI1X7sFT2raEC9UOf4feGdS8bg5uG2r8TMtFag=";
+    }
+  ];
+}
