0ae1917081c5a6818e2f79bca15e13e3392ed94f
Julien Malka <julien@malka.sh>
2024-09-02 14:25:52 +0200

    fix: add patch for swaylock-effects

--- patches/default.nix
+++ patches/default.nix
@@ -10,5 +10,10 @@
       id = "338841";
       hash = "sha256-IgxbVYn8lVUowcTM/v8dBTdptsivg6biDC+jQWFsaQY=";
     }
+    {
+      # swaylock-effects: fix build
+      id = "338876";
+      hash = "sha256-OAUGN6i7Yp8c8eVRpXUBEb/zupuKYE0mmhNVT3Vjy8E=";
+    }
   ];
 }
