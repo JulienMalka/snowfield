42d3a9b865dae95ec46720ee09d4ade6ea1459c0
Julien Malka <julien@malka.sh>
2024-09-10 15:13:48 +0200

    chore: dropping unecessary patches

--- patches/default.nix
+++ patches/default.nix
@@ -1,19 +1,2 @@
 {
-  "nixos-unstable" = [
-    {
-      # xdg-desktop-portal-hyprland: fix build
-      id = "338836";
-      hash = "sha256-QVKF0UIYK5uAq3w1w+9kWwmm1bvMQl7PMgeZ5FkSUFs=";
-    }
-    {
-      # hyprpaper: fix build
-      id = "338841";
-      hash = "sha256-IgxbVYn8lVUowcTM/v8dBTdptsivg6biDC+jQWFsaQY=";
-    }
-    {
-      # swaylock-effects: fix build
-      id = "338876";
-      hash = "sha256-OAUGN6i7Yp8c8eVRpXUBEb/zupuKYE0mmhNVT3Vjy8E=";
-    }
-  ];
 }
