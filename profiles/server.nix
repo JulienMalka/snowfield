b4d2f406661c6912a34fde82e48804c444ee9632
Julien Malka <julien@malka.sh>
2024-11-02 00:49:31 +0100

    feat(servers): add motd

--- profiles/server.nix
+++ profiles/server.nix
@@ -12,4 +12,22 @@
     url = "https://arkheon.luj.fr";
   };
 
+  # Nice motd
+  programs.rust-motd = {
+    enable = true;
+    order = [
+      "filesystems"
+      "memory"
+      "last_login"
+      "uptime"
+    ];
+    settings = {
+      uptime.prefix = "Up";
+      filesystems.root = "/";
+      memory.swap_pos = "below";
+      last_login.root = 3;
+      last_login.julien = 3;
+    };
+  };
+
 }
