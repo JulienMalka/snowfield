aa8a46e8f109d892a10ad02a55cc3793e488517e
Julien Malka <julien@malka.sh>
2024-12-08 00:07:57 +0100

    chore: ditch arkheon

--- profiles/server.nix
+++ profiles/server.nix
@@ -1,16 +1,5 @@
-{ config, ... }:
 {
   deployment.tags = [ "server" ];
-
-  # Enable arkheon
-  age.secrets."arkheon-token".file = ../secrets/arkheon-token.age;
-  services.arkheon.record = {
-    enable = true;
-
-    tokenFile = config.age.secrets."arkheon-token".path;
-
-    url = "https://arkheon.luj.fr";
-  };
 
   # Nice motd
   programs.rust-motd = {
