4a37e0cc99958d490424b8718b1abd9369ec95f3
Julien Malka <julien@malka.sh>
2025-03-13 00:15:46 +0100

    chore: some fix to gustave

--- machines/gustave/home-julien.nix
+++ machines/gustave/home-julien.nix
@@ -2,44 +2,22 @@
 {
   luj.hmgr.julien = {
 
+    luj.emails.enable = true;
+    luj.programs.fish.enable = true;
+    systemd.user.startServices = "sd-switch";
+
     home.persistence."/persistent/home/julien" = {
-      files = [
-        ".config/gnome-initial-setup-done"
-        ".config/background"
-        ".cert/nm-openvpn/telecom-paris-ca.pem"
-        ".local/share/com.ranfdev.Notify.sqlite"
-      ];
       directories = [
-        "Pictures"
-        "Documents"
         ".ssh"
-        ".mozilla"
-        "devold"
-        ".config/cosmic"
         ".local/share/direnv"
-        ".local/state/cosmic-comp"
-        ".local/share/atuin"
-        ".local/share/firefoxpwa"
-        ".config/Signal"
-        ".cache/spotify"
-        ".config/spotify"
-        ".config/autostart"
-        ".config/borg"
-        ".config/pika-backup"
-        ".config/Element"
-        ".step"
-        ".emacs.d"
         ".gnupg"
-        "Zotero"
-        ".config/dconf"
         ".local/share/keyrings"
-        ".cache/mu"
         "Maildir"
       ];
       allowOther = true;
     };
 
     home.stateVersion = "23.11";
-    home.packages = [ pkgs.hello ];
+    home.packages = [ pkgs.muchsync ];
   };
 }
