ce6e0f69af06485256ffb3e802e9284582ef5cb3
Julien Malka <julien@malka.sh>
2025-04-26 12:56:41 +0200

    chore: move gustave to ext4

--- machines/gustave/hardware.nix
+++ machines/gustave/hardware.nix
@@ -1,4 +1,9 @@
-{ lib, modulesPath, ... }:
+{
+  lib,
+  modulesPath,
+  pkgs,
+  ...
+}:
 
 {
   imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
@@ -15,32 +20,13 @@
   boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
 
-  fileSystems."/persistent".neededForBoot = lib.mkForce true;
-
-  boot.initrd.postDeviceCommands = lib.mkAfter ''
-    mkdir /btrfs_tmp
-    mount /dev/disk/by-partlabel/disk-sda-root /btrfs_tmp
-    if [[ -e /btrfs_tmp/root ]]; then
-        mkdir -p /btrfs_tmp/old_roots
-        timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
-        mv /btrfs_tmp/root "/btrfs_tmp/old_roots/$timestamp"
-    fi
-
-    delete_subvolume_recursively() {
-        IFS=$'\n'
-        for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
-            delete_subvolume_recursively "/btrfs_tmp/$i"
-        done
-        btrfs subvolume delete "$1"
-    }
-
-    for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +30); do
-        delete_subvolume_recursively "$i"
-    done
-
-    btrfs subvolume create /btrfs_tmp/root
-    umount /btrfs_tmp
+  boot.initrd.postDeviceCommands = ''
+    lvm lvremove --force /dev/mainpool/root || :
+    yes | lvm lvcreate --size 100G --name root mainpool
+    ${pkgs.e2fsprogs}/bin/mkfs.ext4 /dev/mainpool/root
   '';
+
+  fileSystems."/persistent".neededForBoot = lib.mkForce true;
 
   networking.useDHCP = lib.mkDefault true;
 
