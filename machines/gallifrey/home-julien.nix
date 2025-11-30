fc71842f580c9fa15861561be6f7f22bc86f5f49
Julien Malka <julien@malka.sh>
2025-11-30 21:11:45 +0100

    fix: downgrade obs

--- machines/gallifrey/home-julien.nix
+++ machines/gallifrey/home-julien.nix
@@ -52,7 +52,7 @@
 
     programs.obs-studio = {
       enable = true;
-      plugins = with pkgs; [ obs-studio-plugins.obs-vkcapture ];
+      package = pkgs.stable.obs-studio;
     };
 
     programs.emacs = {
