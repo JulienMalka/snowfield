bc728627ce10bea52db77e609ae28be7c9f08d73
Julien Malka <julien@malka.sh>
2026-02-23 15:18:28 +0100

    chore(shell): agenix -> ragenix

--- shell.nix
+++ shell.nix
@@ -2,9 +2,22 @@ let
   inputs = import ./lon.nix;
   pkgs = import inputs.unstable { };
   nixos-anywhere = pkgs.callPackage "${inputs.nixos-anywhere}/src/default.nix" { };
-  agenix = pkgs.callPackage "${inputs.agenix}/pkgs/agenix.nix" { };
+  ragenixSrc = builtins.fetchGit {
+    url = "git+ssh://git@github.com/geosurge-ai/ragenix.git";
+    rev = "7bcb863c1ca86bac082f7a46c9ba945a9b4bbeb9";
+    allRefs = true;
+  };
+  ragenix = pkgs.ragenix.overrideAttrs (_: {
+    src = ragenixSrc;
+    cargoDeps = pkgs.rustPlatform.fetchCargoVendor {
+      src = ragenixSrc;
+      hash = "sha256-liimzFjEqgwB15VUfcwu1CRFEeDyXJ6fsH3pzfUPeKo=";
+    };
+  });
   bootstrap = pkgs.callPackage scripts/bootstrap-machine.nix { inherit nixos-anywhere; };
+  snowfield = pkgs.callPackage scripts/snowfield.nix { };
   lon = pkgs.callPackage "${inputs.lon}/nix/packages/lon.nix" { };
+  ci = import ./ci.nix;
   pre-commit-hook =
     (import (
       pkgs.applyPatches {
@@ -37,13 +50,15 @@ pkgs.mkShell {
   nativeBuildInputs = [
     pkgs.colmena
     pkgs.rbw
-    agenix
+    ragenix
     bootstrap
+    snowfield
     pkgs.statix
     lon
   ];
   shellHook = ''
     ${pre-commit-hook.shellHook}
+    ${ci.workflowInstall.shellHook}
     repo_root="$(git rev-parse --show-toplevel 2>/dev/null || printf '%s' "$PWD")"
     export RULES="$repo_root/private/secrets/secrets.nix"
   '';
