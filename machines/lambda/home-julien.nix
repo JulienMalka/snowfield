4f40fa4463a147a76a08f3d50bf3301e17500017
Julien Malka <julien@malka.sh>
2026-03-09 23:52:02 +0100

    chore(lambda): remove neovim

--- machines/lambda/home-julien.nix
+++ machines/lambda/home-julien.nix
@@ -1,8 +1,6 @@
-_:
-{
+_: {
   luj.hmgr.julien = {
     home.stateVersion = "22.11";
-    luj.programs.neovim.enable = true;
     luj.programs.ssh-client.enable = true;
     luj.programs.git.enable = true;
   };
