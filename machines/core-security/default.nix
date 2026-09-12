ba3814fb01bf5594424888074606503e88535559
Julien Malka <julien@malka.sh>
2026-09-12 15:08:57 +0200

    feat(core-security): new public addresses at Hetzner

--- machines/core-security/default.nix
+++ machines/core-security/default.nix
@@ -25,10 +25,10 @@
       monitoring
     ];
     ips = {
-      public.ipv4 = "82.67.34.230";
-      local.ipv4 = "192.168.0.175";
+      public.ipv4 = "77.42.114.11";
+      local.ipv4 = "192.168.0.143";
       vpn.ipv4 = "100.100.45.14";
-      public.ipv6 = "2a01:e0a:de4:a0e1:40f0:8cff:fe31:3e94";
+      public.ipv6 = "2a01:4f9:3090:2b8c:40f0:8cff:fe31:3e94";
       vpn.ipv6 = "fd7a:115c:a1e0::e";
     };
   };
