4a94b900c5b1666c71b2bcd422c5efdba4a46739
Julien Malka <julien@malka.sh>
2026-09-13 10:22:25 +0200

    feat(tower): new public IPv6 at Hetzner

--- machines/tower/default.nix
+++ machines/tower/default.nix
@@ -26,7 +26,7 @@
       public.ipv4 = "77.42.114.11";
       local.ipv4 = "192.168.0.103";
       vpn.ipv4 = "100.100.45.9";
-      public.ipv6 = "2a01:e0a:de4:a0e1:8ec7:b5d2:f663:a67a";
+      public.ipv6 = "2a01:4f9:3090:2b8c:8ec7:b5d2:f663:a67a";
       vpn.ipv6 = "fd7a:115c:a1e0::9";
     };
 
